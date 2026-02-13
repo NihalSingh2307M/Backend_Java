828c2769d400d9f47f198cc7565763e2e355af69
NihalSingh <nihalsingh2307n@gmail.com>
2026-02-13 14:49:21 +0530

    interface-class-abstract

---
+++ src/AbstractionClass.java
@@ -0,0 +1,11 @@
+//interface -diff vs class
+//method structure -sighnature body
+
+
+
+
+public class AbstractionClass {
+    public static void main(String [] args){
+
+    }
+}
--- src/Oops.java
+++ src/Oops.java
@@ -1,20 +1,38 @@
-class User{
-    int id;
-    String email;
-    String password;
-    float progress;
-    User(int id,String email,String password,float progress){
-        this.id= id;
-        this.email=email;
-        this.password=password;
+class User {
+    private int id;
+    private String email;
+    private String password;
+    private float progress;
+
+    User(String email, String password) {
+        this.email = email;
+        this.password = password;
+    }
+
+    User(int id, String email, String password, float progress) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
         this.progress = progress;
     }
-}
+    //rules to remember
+    //getter method ->to get the data
+    //class-abstractclass-interface diff to token level
 
-public class Oops {
-    public static  void main(String[]args){
-        User userOne =new User(1,"nihal208@gmail.com","123445",80.0f);
-        System.out.println("user email is:" +userOne.email);
+    public int getId() {
+        return this.id;
     }
 
+    //setter method-> to set data
+    public int getId(int id) {
+        return this.id = id;
+    }
+
+    public class Oops {
+        public static void main(String[] args) {
+            User userOne = new User(1, "nihal208@gmail.com", "123445", 80.0f);
+            System.out.println("user email is:" + userOne.email);
+        }
+
+    }
 }
